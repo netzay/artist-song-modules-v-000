0cfa6b450caaf57dd130b7e6d03f64e689a4763d
Ally Netzel <netzelally@gmail.com>
2018-04-19 01:29:03 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,8 @@
 module Findable
 
   def find_by_name(name)
-    artist.detect{|a| a.name == name}
+    all.detect{|a| a.name == name}
   end  
   
-  def find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
   
 end
